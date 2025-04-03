2e1c4709c0c00900e4958c3fe75939bb28a0bb54
Arul A <nareshlaesn170705@gmail.com>
2025-04-03 21:00:19 +0530

    3-4-25

---
+++ T14.java
@@ -0,0 +1,103 @@
+/*Java Substring */
+
+/*Given a string, , and two indices,  and , print a substring consisting of all characters in the inclusive range from  to . You'll find the String class' substring method helpful in completing this challenge.
+
+Input Format
+
+The first line contains a single string denoting .
+The second line contains two space-separated integers denoting the respective values of  and .
+
+Constraints
+
+String  consists of English alphabetic letters (i.e., ) only.
+Output Format
+
+Print the substring in the inclusive range from  to .
+
+Sample Input
+
+Helloworld
+3 7
+Sample Output
+
+lowo
+Explanation
+
+In the diagram below, the substring is highlighted in green: */
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        for(int i=start;i<end;i++)
+        {
+            System.out.print(S.charAt(i));
+        }
+    }
+}
+
+
+/*Java Primality Test */
+
+/*A prime number is a natural number greater than  whose only positive divisors are  and itself. For example, the first six prime numbers are , , , , , and .
+
+Given a large integer, , use the Java BigInteger class' isProbablePrime method to determine and print whether it's prime or not prime.
+
+Input Format
+
+A single line containing an integer,  (the number to be checked).
+
+Constraints
+
+ contains at most  digits.
+Output Format
+
+If  is a prime number, print prime; otherwise, print not prime.
+
+Sample Input
+
+13
+Sample Output
+
+prime
+Explanation
+
+The only positive divisors of  are  and , so we print prime.
+
+ */
+
+ import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = bufferedReader.readLine();
+        BigInteger num=new BigInteger(n);
+        if(num.isProbablePrime(10))
+        {
+            System.out.print("prime");
+        }
+        else {
+            System.out.print("not prime");
+        }
+        bufferedReader.close();
+    }
+}
